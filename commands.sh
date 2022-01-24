af1b07aef240cd634de0750660da62711401b1c3
nandoplayart <41376315+nandoplayart@users.noreply.github.com>
2022-01-24 07:41:29 -0300

    Create commands.sh

---
+++ commands.sh
@@ -0,0 +1,6 @@
+sudo su
+curl https://releases.rancher.com/install-docker/19.03.sh | sh
+usermod -aG docker ubuntu
+curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
